18640c8a1e3461476e5940695993a554b42a504a
Advaith-Nair <103027330+Advaith-Nair@users.noreply.github.com>
2022-04-08 09:28:24 +0530

    Update pneumonia_classification.py

--- pneumonia_classification.py
+++ pneumonia_classification.py
@@ -11,8 +11,8 @@ def prepare(img):
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     image = img
     #image sizing
-    size = 224
-    image = ImageOps.fit(image,size,size, 3)
+    size = (224, 224)
+    image = ImageOps.fit(image,size, 3)
 
     #Next, turn the image into a numpy array
     image_array = np.ndarray(image, shape = (1,224,224,3),dtype = np.float32)
