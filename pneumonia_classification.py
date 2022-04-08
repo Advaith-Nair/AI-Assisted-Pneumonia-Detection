0671d5c487acc359fec89f0dbd90f59bcd51ad24
Advaith-Nair <103027330+Advaith-Nair@users.noreply.github.com>
2022-04-08 09:31:34 +0530

    Update pneumonia_classification.py

--- pneumonia_classification.py
+++ pneumonia_classification.py
@@ -1,52 +1,37 @@
-import os
 import cv2
 import tensorflow as tf
 import streamlit as st
-from PIL import Image, ImageOps
+from PIL import Image
 import time
-import numpy as np
 CATEGORIES = ["NORMAL", "PNEUMONIA"]
-
-def prepare(img):
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
-    size = (224, 224)
-    image = ImageOps.fit(image,size, 3)
-
-    #Next, turn the image into a numpy array
-    image_array = np.ndarray(image, shape = (1,224,224,3),dtype = np.float32)
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run it
-    prediction = model.predict(data)
-    return np.argmax(prediction)
+relpath = r"C:\Users\Advaith Nair\OneDrive\Desktop\Advaith\School\Grade XII\AI\Bootcamp\AI Assisted Medical Diagnosis\CNN Pneumonia-2.h5"
+def prepare(filepath):
+    IMG_SIZE = 64
+    img_array = cv2.imread(filepath,cv2.IMREAD_GRAYSCALE)
+    img_array = cv2.resize(img_array,(IMG_SIZE,IMG_SIZE))
+    return img_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
 
+model = tf.keras.models.load_model(r"/app/ai-assisted-pneumonia-detection/CNN Pneumonia-2.h5")
 model = tf.keras.models.load_model("CNN Pneumonia-2")
 st.title('Hello this model aims at identifying pneumonia by looking at X-rays.')
 uploaded_file = st.file_uploader(label="Please upload your X-Ray", type=["JPEG", "JPG", "PNG"])
 if uploaded_file is not None:
     try:
+        path = st.text_input('Add the path to your image without quotes')
         image = Image.open(uploaded_file)
         st.image(image, caption='This is your uploaded file')
-        predicted_val = prepare(image)
-        if predicted_val == 0:
-            st.title('NO ISSUE DETECTED.')
-            st.write('Lungs seem to be healthy.')
-        else:
-            st.title('PNEUMONIA DETECTED.')
-            st.write('Please consult a medical expert.')
+        prediction = model.predict([prepare(path)])
+        outcome = (CATEGORIES[int(prediction[0][0])])
+        if outcome =='PNEUMONIA':
+            st.write('PNEUMONIA DETECTED. Please consult a medical expert')
+        elif outcome == 'NORMAL':
+            st.write('NO ISSUE DETECTED. Lungs seem to be normal.')
     
     except Exception as e:
-        time.sleep(5)
+        time.sleep(10)
         st.write('There was an error. Please try again later or refresh the page.'
                 ' Make sure to enter the data accurately and put the path WITHOUT quotes')
-        st.write(e)
 else:
     print()
