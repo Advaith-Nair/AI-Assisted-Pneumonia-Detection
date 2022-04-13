d4377fa6f21b8585e47b1e213527b364e88f1cfa
Advaith-Nair <103027330+Advaith-Nair@users.noreply.github.com>
2022-04-13 11:01:52 +0530

    Update pneumonia_classification.py

--- pneumonia_classification.py
+++ pneumonia_classification.py
@@ -11,16 +11,15 @@ def prepare(file):
     img_array = np.array(file)
     img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     return img_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-
+if info:
+            st.write('Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia.')
+            st.write("Click this link to know more: https://www.who.int/news-room/fact-sheets/detail/pneumonia")
 st.set_page_config(page_title='X-Classifier - Pneumonia Detector', page_icon='❎')
 model = tf.keras.models.load_model('CNN Pneumonia-2')
 st.title('Hello this model aims at identifying pneumonia by looking at X-rays.')
 uploaded_file = st.file_uploader(label="Please upload your X-Ray", type=["JPEG", "JPG", "PNG"])
 if uploaded_file is not None:
     try: 
-         if info:
-            st.write('Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia.')
-            st.write("Click this link to know more: https://www.who.int/news-room/fact-sheets/detail/pneumonia")
         image = Image.open(uploaded_file)
         st.image(image, caption='This is your uploaded file')
         prediction = model.predict([prepare(image)])
